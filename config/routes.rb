8e01e9e47e60fae17b124b78a0fa80d3b0cc8f17
EC2 Default User <u6cfdm3p@gmail.com>
2022-02-08 13:02:25 +0000

    [Update]showへの遷移完了、コントローラ復習必須

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/' => 'homes#top'
   get 'books/new'
   get 'books' => 'books#index', as: 'books'
+  post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
