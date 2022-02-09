3c033b4f207d04c1825c643224e5c97bdfd10345
EC2 Default User <u6cfdm3p@gmail.com>
2022-02-09 01:14:00 +0000

    [Update]ストロングパラメーター引数の挙動確認

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'books' => 'books#index', as: 'books'
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book'
-  get 'books/edit'
+  get 'books/:id/edit' => 'books#edit',as: 'edit_book'
+  patch 'books/:id/edit' => 'books#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
